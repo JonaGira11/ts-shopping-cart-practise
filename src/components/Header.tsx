c94e9b0a8ab4b29934b8152bc05b0d0af0f74864
Jonthesallz <gitboi56@outlook.com>
2023-01-21 20:28:13 +0000

    adding more features. building the site

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import Nav from "./Nav"
+import useCart from "../hooks/useCart"
+
+type HeaderPropsType = {
+  viewCart: boolean,
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const Header = ({ viewCart, setViewCart }: HeaderPropsType) => {
+  const {totalItems, totalPrice} = useCart()
+
+  const content = (
+    <header className="header">
+      <div className="header__title-bar">
+        <h1>Acme Co.</h1>
+        <div className="header_price-box">
+          <p>Total items:{totalItems}</p>
+          <p>Total Price:{totalPrice}</p>
+        </div>
+      </div>
+      <Nav viewCart={viewCart} setViewCart={setViewCart} />
+    </header>
+  )
+  return (
+    content
+  )
+}
+
+export default Header
