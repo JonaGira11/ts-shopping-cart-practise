deccda388172af441e164354df3a131084fa6240
Jonthesallz <gitboi56@outlook.com>
2023-01-26 22:10:57 +0000

    optimised

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -5,34 +5,33 @@ import { ReactElement } from "react"
 import Product from "./Product"
 
 const ProductList = () => {
+    const { dispatch, REDUCER_ACTIONS, cart } = useCart()
+    const { products } = useProducts()
 
-  const {dispatch, REDUCER_ACTIONS, cart} = useCart()
-  const {products} = useProducts()
+    let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>
 
-  let pageContent : ReactElement | ReactElement[] = <p>Loading...</p>
+    if (products?.length) {
+        pageContent = products.map(product => {
+            const inCart: boolean = cart.some(item => item.sku === product.sku)
 
-  if (products?.length) {
-    pageContent =  products.map(product => {
-      const inCart: boolean = cart.some(item => item.sku === product.sku )
+            return (
+                <Product
+                    key={product.sku}
+                    product={product}
+                    dispatch={dispatch}
+                    REDUCER_ACTIONS={REDUCER_ACTIONS}
+                    inCart={inCart}
+                />
+            )
+        })
+    }
 
-      return (
-        <Product key={product.sku} 
-        product={product}
-        dispatch={dispatch}
-        REDUCER_ACTIONS={REDUCER_ACTIONS}
-        inCart={inCart}
-        />
-      )
-    })
-  }
+    const content = (
+        <main className="main main--products">
+            {pageContent}
+        </main>
+    )
 
-  const content = (
-    <main className="main main--product">
-      {pageContent}
-    </main>
-  )
-
-  return content
+    return content
 }
-
 export default ProductList
